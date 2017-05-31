7cc44502bd78e95a3c284e8c0bdbcd7fc730f4da
Homay0118 <z_hongmei@sina.cn>
2017-05-31 18:39:09 +0800

    change

--- Test/Middleburytest_2014.m
+++ Test/Middleburytest_2014.m
@@ -86,8 +86,7 @@ P1=1.2;
 sigma=0.4;
 ErrorRate = zeros(1,15);
 %--------------------------------- Executing ------------------------------%
-% for im_num = test_start: test_end   % if all the datasets are downloaded.
-for im_num =2:2
+for im_num = test_start: test_end   % if all the datasets are downloaded.
     %----------- Adjust the range of disparities to the chosen resolution -------------------------------%
     if imgsize == 'Q'
         DisparityRange = [1,round(ndisp(im_num)/4)];
